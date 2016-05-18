7824af5c409227edc0f12d3651c1ee95c1f95699
Christopher Robert Philabaum <chaos7theory@live.com>
2016-05-17 23:46:35 -0700

    Added pins and bowling ball.
    Added basis for bowling ball game.

--- src/js/game.js
+++ src/js/game.js
@@ -4,30 +4,47 @@
     document.addEventListener("DOMContentLoaded", (event) => {
         const WIDTH = 400;
         const HEIGHT = 400;
+        const PIXEL_SIZE = 32;
 
         var gameport = document.getElementById("gameport");
         // Shortened from #3344ee
-        var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, { backgroundColor: 0x34e });
+        var renderer = PIXI.autoDetectRenderer(WIDTH, HEIGHT, { backgroundColor: 0x000 });
         gameport.appendChild(renderer.view);
 
         var stage = new PIXI.Container();
-        var texture = PIXI.Texture.fromImage('assets/check.png');
-        var sprite = new PIXI.Sprite(texture);
+        var ball = new PIXI.Sprite(PIXI.Texture.fromImage('assets/bowl_ball.png'));
+        var pins = new Array();
+        const PIN_IMG = PIXI.Texture.fromImage('assets/pin.png');
+        for(let i = 0; i < 10; i++) {
+            pins.push(new PIXI.Sprite(PIN_IMG));
+        }
 
-        sprite.anchor.x = 0.5;
-        sprite.anchor.y = 0.5;
-
+        ball.anchor.x = 0.5;
+        ball.anchor.y = 0.5;
         // Center sprite on screen.
-        sprite.position.x = WIDTH / 2;
-        sprite.position.y = HEIGHT / 2;
+        ball.position.x = WIDTH / 2;
+        ball.position.y = HEIGHT - PIXEL_SIZE;
+        stage.addChild(ball);
 
-        stage.addChild(sprite);
+        for(let p = 0, row = 0; p < pins.length; p++) {
+            pins[p].anchor.x = 0.5;
+            pins[p].anchor.y = 0.5;
+
+            pins[p].position.x = WIDTH / 2;
+            pins[p].position.y = (p + 1) * PIXEL_SIZE;
+
+            stage.addChild(pins[p]);
+        }
 
         // Self-execute animate
         (function animate() {
             requestAnimationFrame(animate);
-            sprite.rotation += 0.1;
+            ball.rotation += 0.1;
             renderer.render(stage);
         })();
     });
+
+    function isLastOfRow(pin, row) {
+        return pin === row;
+    }
 })();
